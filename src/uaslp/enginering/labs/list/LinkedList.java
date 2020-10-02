5146ac24df76922c9e22d2802c72687b0c099429
JEdilbertoTC <joseedilberto367@outlook.com>
2020-10-02 14:23:04 -0500

    src, test

---
+++ src/uaslp/enginering/labs/list/LinkedList.java
@@ -0,0 +1,216 @@
+package uaslp.enginering.labs.list;
+
+import uaslp.enginering.labs.list.model.Student;
+
+public class LinkedList {
+
+    private int lastIndex;
+    private Node front;
+    private Node tail;
+
+    public enum Position {
+        BEFORE,
+        AFTER
+    }
+
+    public class Iterator {
+
+        private Node node = front;
+
+        public boolean hasNext() {
+            return node != null;
+        }
+
+        public Student next() {
+            Node currentNode = node;
+            if (currentNode != null) {
+                node = node.getNext();
+                return currentNode.getStudent();
+            }
+            return null;
+        }
+    }
+
+    public void print() {
+        Node temp = front;
+        while (temp != null) {
+            System.out.println(temp.getStudent().getName());
+            temp = temp.getNext();
+        }
+    }
+
+    public void print2() {
+        while (tail != null) {
+            System.out.println(tail.getStudent().getName());
+            tail = tail.getPrevious();
+        }
+    }
+
+    public void add(Student student) {
+        Node newNode = new Node(student);
+        if (front == null && tail == null) {
+            front = tail = newNode;
+        } else {
+            Node temp = front;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(newNode);
+            newNode.setPrevious(temp);
+            tail = newNode;
+        }
+        lastIndex++;
+    }
+
+    public void delete(Student student) {
+        if (front.equals(tail)) front = tail = null;
+        else {
+            Node temp = front;
+            while (temp.getNext() != null && !temp.getStudent().equals(student)) {
+                temp = temp.getNext();
+            }
+            if (temp.equals(front)) {
+                deleteFront();
+                return;
+            } else if (temp.equals(tail)) {
+                deleteTail();
+                return;
+            }
+            Node temp2 = temp.getPrevious();
+            temp2.setNext(temp.getNext());
+            Node temp3 = temp.getNext();
+            temp3.setPrevious(temp.getPrevious());
+            /*
+            temp.setNext(temp.getPrevious());
+            temp.setPrevious(temp.getNext());*/
+            lastIndex--;
+        }
+
+    }
+
+    private void deleteFront() {
+        front = front.getNext();
+    }
+
+    private void deleteTail() {
+        tail = tail.getPrevious();
+        tail.setNext(null);
+    }
+
+    public void delete(int index) {
+            if (index <= lastIndex && index>=0) {
+                Node aux = front;
+                int count = 0;
+                while (aux.getNext() != null && index != count) {
+                    aux = aux.getNext();
+                    count++;
+                }
+                if (aux.equals(front)) {
+                    deleteFront();
+                    lastIndex--;
+                    return;
+                } else if (aux.equals(tail)) {
+                    deleteTail();
+                    lastIndex--;
+                    return;
+                }
+                Node aux2 = aux.getPrevious();
+                aux2.setNext(aux.getNext());
+                Node aux3 = aux.getNext();
+                aux3.setPrevious(aux.getPrevious());
+                lastIndex--;
+            }
+
+    }
+
+    public Iterator getIterator() {
+        return new Iterator();
+    }
+
+    public int size() {
+        return lastIndex;
+    }
+
+    public Student getAt(int index) {
+        if (index < lastIndex) {
+            Node aux = front;
+            int count = 0;
+            while (aux.getNext() != null && index != count) {
+                aux = aux.getNext();
+                count++;
+            }
+            return aux.getStudent();
+        }
+        return null;
+    }
+
+    public void insert(Student reference, Student newStudent, Position position) {
+        Node referenced = new Node(reference);
+        Node newNode = new Node(newStudent);
+        if (front != null) {
+            Node temp = front;
+            while (temp.getNext() != null && !temp.getStudent().getName().equals(reference.getName())) {
+                temp = temp.getNext();
+            }
+            if (temp.equals(front)) {
+                if (position.equals(Position.BEFORE)) {
+                    front = newNode;
+                    newNode.setNext(temp);
+                    temp.setPrevious(newNode);
+                    lastIndex++;
+                    return;
+                } else {
+                    Node temp2 = temp.getNext();
+                    newNode.setNext(temp2);
+                    newNode.setPrevious(temp);
+                    temp.setNext(newNode);
+                    //temp2.setPrevious(newNode);
+                    lastIndex++;
+                    return;
+                }
+            }
+            if (temp.equals(tail)) {
+                if (position.equals(Position.AFTER)) {
+                    newNode.setPrevious(temp);
+                    temp.setNext(newNode);
+                    tail = newNode;
+                    lastIndex++;
+                    return;
+                } else {
+                    Node temp2 = temp.getPrevious();
+                    newNode.setNext(temp);
+                    temp2.setNext(newNode);
+                    newNode.setPrevious(temp2);
+                    temp.setPrevious(newNode);
+                    lastIndex++;
+                    return;
+                }
+            }
+            if (position.equals(Position.AFTER)) {
+                Node temp2 = temp.getNext();
+                newNode.setNext(temp2);
+                temp.setNext(newNode);
+                newNode.setPrevious(temp);
+                lastIndex++;
+                return;
+            } else {
+                Node temp2 = temp.getPrevious();
+                newNode.setNext(temp);
+                newNode.setPrevious(temp2);
+                temp2.setNext(newNode);
+                temp.setPrevious(newNode);
+                lastIndex++;
+                return;
+            }
+        }
+        lastIndex++;
+    }
+
+    public Node getFront() {
+        return front;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+}
---
+++ test/uaslp/enginering/labs/list/LinkedListTest.java
@@ -0,0 +1,289 @@
+package uaslp.enginering.labs.list;
+
+import org.junit.jupiter.api.Test;
+import uaslp.enginering.labs.list.model.Student;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LinkedListTest{
+
+
+    @Test
+    public void givenNewList_whenSize_thenZeroIsReturned() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+
+        // When:
+        int size = linkedList.size();
+
+        // Then:
+        assertEquals(0, size);
+    }
+
+    @Test
+    public void givenNewList_whenAdd_thenElementIsInserted() {
+        // Given:
+        LinkedList LinkedList = new LinkedList();
+        Student student = new Student("Ivan");
+
+        // When:
+        LinkedList.add(student);
+
+        // Then:
+        assertEquals(1, LinkedList.size());
+        assertEquals(LinkedList.getAt(0).getName(), "Ivan");
+    }
+
+
+    @Test
+    public void givenAListWith3Elements_whenDeleteFirst_thenElementIsDeleted() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.add(new Student("Ivan"));
+        linkedList.add(new Student("Israel"));
+        linkedList.add(new Student("Francisco"));
+
+        // When:
+        linkedList.delete(0);
+
+        // Then:
+        assertEquals(2, linkedList.size());
+        assertEquals(linkedList.getAt(0).getName(), "Israel");
+        assertEquals(linkedList.getAt(1).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenDeleteLast_thenElementIsDeleted() {
+        // Given:
+        LinkedList LinkedList = new LinkedList();
+
+        LinkedList.add(new Student("Ivan"));
+        LinkedList.add(new Student("Israel"));
+        LinkedList.add(new Student("Francisco"));
+
+        // When:
+        LinkedList.delete(2);
+
+        // Then:
+        assertEquals(2, LinkedList.size());
+        assertEquals(LinkedList.getAt(0).getName(), "Ivan");
+        assertEquals(LinkedList.getAt(1).getName(), "Israel");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenDeleteMiddle_thenElementIsDeleted() {
+        // Given:
+        LinkedList LinkedList = new LinkedList();
+
+        LinkedList.add(new Student("Ivan"));
+        LinkedList.add(new Student("Israel"));
+        LinkedList.add(new Student("Francisco"));
+
+        // When:
+        LinkedList.delete(1);
+
+        // Then:
+        assertEquals(2, LinkedList.size());
+        assertEquals(LinkedList.getAt(0).getName(), "Ivan");
+        assertEquals(LinkedList.getAt(1).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenDeleteNegative_thenDoesNothing() {
+        // Given:
+        LinkedList LinkedList = new LinkedList();
+
+        LinkedList.add(new Student("Ivan"));
+        LinkedList.add(new Student("Israel"));
+        LinkedList.add(new Student("Francisco"));
+
+        // When:
+        LinkedList.delete(-1);
+
+        // Then:
+        assertEquals(3, LinkedList.size());
+        assertEquals(LinkedList.getAt(0).getName(), "Ivan");
+        assertEquals(LinkedList.getAt(1).getName(), "Israel");
+        assertEquals(LinkedList.getAt(2).getName(), "Francisco");
+    }
+    //corregir
+    @Test
+    public void givenAListWith3Elements_whenDeleteOutOfSize_thenDoesNothing() {
+        // Given:
+        LinkedList LinkedList = new LinkedList();
+
+        LinkedList.add(new Student("Ivan"));
+        LinkedList.add(new Student("Israel"));
+        LinkedList.add(new Student("Francisco"));
+
+        // When:
+        LinkedList.delete(4);
+
+        // Then:
+        assertEquals(3, LinkedList.size());
+        assertEquals(LinkedList.getAt(0).getName(), "Ivan");
+        assertEquals(LinkedList.getAt(1).getName(), "Israel");
+        assertEquals(LinkedList.getAt(2).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenInsertAtBeginningBefore_thenElementIsInserted() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+        Student reference = new Student("Ivan");
+
+        linkedList.add(reference);
+        linkedList.add(new Student("Israel"));
+        linkedList.add(new Student("Francisco"));
+
+        // When:
+        linkedList.insert(reference, new Student("Lupita"), LinkedList.Position.BEFORE);
+
+        // Then:
+        assertEquals(4, linkedList.size());
+        assertEquals(linkedList.getAt(0).getName(), "Lupita");
+        assertEquals(linkedList.getAt(1).getName(), "Ivan");
+        assertEquals(linkedList.getAt(2).getName(), "Israel");
+        assertEquals(linkedList.getAt(3).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenInsertAtEndBefore_thenElementIsInserted() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+        Student reference = new Student("Francisco");
+
+        linkedList.add(new Student("Ivan"));
+        linkedList.add(new Student("Israel"));
+        linkedList.add(reference);
+
+        // When:
+        linkedList.insert(reference, new Student("Lupita"), LinkedList.Position.BEFORE);
+
+        // Then:
+        assertEquals(4, linkedList.size());
+        assertEquals(linkedList.getAt(0).getName(), "Ivan");
+        assertEquals(linkedList.getAt(1).getName(), "Israel");
+        assertEquals(linkedList.getAt(2).getName(), "Lupita");
+        assertEquals(linkedList.getAt(3).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenInsertAtMiddleBefore_thenElementIsInserted() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+        Student reference = new Student("Israel");
+
+        linkedList.add(new Student("Ivan"));
+        linkedList.add(reference);
+        linkedList.add(new Student("Francisco"));
+
+        // When:
+        linkedList.insert(reference, new Student("Lupita"), LinkedList.Position.BEFORE);
+
+        // Then:
+        assertEquals(4, linkedList.size());
+        assertEquals(linkedList.getAt(0).getName(), "Ivan");
+        assertEquals(linkedList.getAt(1).getName(), "Lupita");
+        assertEquals(linkedList.getAt(2).getName(), "Israel");
+        assertEquals(linkedList.getAt(3).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenInsertAtBeginningAfter_thenElementIsInserted() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+        Student reference = new Student("Ivan");
+
+        linkedList.add(reference);
+        linkedList.add(new Student("Israel"));
+        linkedList.add(new Student("Francisco"));
+
+        // When:
+        linkedList.insert(reference, new Student("Lupita"), LinkedList.Position.AFTER);
+
+        // Then:
+        assertEquals(4, linkedList.size());
+        assertEquals(linkedList.getAt(0).getName(), "Ivan");
+        assertEquals(linkedList.getAt(1).getName(), "Lupita");
+        assertEquals(linkedList.getAt(2).getName(), "Israel");
+        assertEquals(linkedList.getAt(3).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenInsertAtEndAfter_thenElementIsInserted() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+        Student reference = new Student("Francisco");
+
+        linkedList.add(new Student("Ivan"));
+        linkedList.add(new Student("Israel"));
+        linkedList.add(reference);
+
+        // When:
+        linkedList.insert(reference, new Student("Lupita"), LinkedList.Position.AFTER);
+
+        // Then:
+        assertEquals(4, linkedList.size());
+        assertEquals(linkedList.getAt(0).getName(), "Ivan");
+        assertEquals(linkedList.getAt(1).getName(), "Israel");
+        assertEquals(linkedList.getAt(2).getName(), "Francisco");
+        assertEquals(linkedList.getAt(3).getName(), "Lupita");
+    }
+
+    @Test
+    public void givenAListWith3Elements_whenInsertAtMiddleAfter_thenElementIsInserted() {
+        // Given:
+        LinkedList linkedList = new LinkedList();
+        Student reference = new Student("Israel");
+
+        linkedList.add(new Student("Ivan"));
+        linkedList.add(reference);
+        linkedList.add(new Student("Francisco"));
+
+        // When:
+        linkedList.insert(reference, new Student("Lupita"), LinkedList.Position.AFTER);
+
+        // Then:
+        assertEquals(4, linkedList.size());
+        assertEquals(linkedList.getAt(0).getName(), "Ivan");
+        assertEquals(linkedList.getAt(1).getName(), "Israel");
+        assertEquals(linkedList.getAt(2).getName(), "Lupita");
+        assertEquals(linkedList.getAt(3).getName(), "Francisco");
+    }
+
+    @Test
+    public void givenEmptyList_whenGetIterator_thenIteratorIsEmpty(){
+        // Given:
+        LinkedList linkedList = new LinkedList();
+
+        // When:
+        LinkedList.Iterator iterator = linkedList.getIterator();
+
+        // Then:
+        assertNotNull(iterator);
+        assertFalse(iterator.hasNext());
+        assertNull(iterator.next());
+    }
+
+    @Test
+    public void givenListWithOneElement_whenGetIterator_thenIteratorHasOneNext(){
+        // Given:
+        LinkedList LinkedList = new LinkedList();
+
+        LinkedList.add(new Student("Ivan"));
+
+        // When:
+        LinkedList.Iterator iterator = LinkedList.getIterator();
+
+        // Then:
+        assertNotNull(iterator);
+        assertTrue(iterator.hasNext());
+        Student student = iterator.next();
+        assertNotNull(student);
+        assertEquals("Ivan", student.getName());
+        assertFalse(iterator.hasNext());
+        assertNull(iterator.next());
+    }
+}
